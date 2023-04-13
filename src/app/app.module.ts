e1e1b68c98d8f2788d2d21052ec815fe9693a3bf
Gitesh Sharma <gitesh15796@gmail.com>
2023-04-13 14:48:37 +0530

    navbar and carousel completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from "./app.component";
+import {BrowserModule} from "@angular/platform-browser";
+import { HomeComponent } from './components/home/home.component';
+import {RouterModule} from "@angular/router";
+import { NavbarComponent } from './components/navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    CommonModule,
+    BrowserModule,
+    RouterModule,
+    AppRoutingModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
