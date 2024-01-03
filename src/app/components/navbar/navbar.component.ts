942dc0d6761967de448d7a5fed107bfef194c70c
Gitesh Sharma <gitesh15796@gmail.com>
2024-01-04 00:04:41 +0530

    hidden link on current page in navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
 
+
+export class NavbarComponent {
+  constructor(private router: Router) {}
+
+  shouldDisplayLink(link: string): boolean {
+    // Check if the current route matches the provided link
+    return this.router.url !== link;
+  }
 }
